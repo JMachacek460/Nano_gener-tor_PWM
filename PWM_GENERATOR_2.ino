1416901cea50c3c500295e6d4c42a68a2b6d79a9
Jaroslav Macháček <jmachacek460@gmail.com>
2025-12-12 21:54:18 +0100

    přidany tlačitka 12 a 11 pro 10x zpomalení a 10x zrychleni

--- PWM_GENERATOR_2.ino
+++ PWM_GENERATOR_2.ino
@@ -3,24 +3,33 @@
  * Popis: Konfiguruje Timer1 pro generovani 50Hz PWM na pinu 9.
  * A1 potenciometr střídy
  * A0 potenciometr frekvence
- * Pin4 tlačitko - pro skokovou změnu PWM (invertuje PWM)
+ * Pin4 přepínač - pro skokovou změnu PWM (invertuje PWM)
+ * Pin12 tlačitko - 10x zpomalí
+ * Pin11 tlačítko - 10x zrychlí
  */
 
 // Pokud odkomentujete nasledujici radek, aktivujete ladici rezim (debug mode)
-#define DEBUG_MODE 
+//#define DEBUG_MODE 
 
-const int potPin = A1;      // Vstup pro potenciometr střídy
-const int potPinPer = A0;      // Vstup pro potenciometr periody
+const int pinStrida = A1;    // Vstup pro potenciometr střídy
+const int pinPerioda = A0;   // Vstup pro potenciometr periody
 const int tlacitko = 4;      // Vstup pro přepínač
+const int PinPWM = 9;
+const int prepinac1 = 12;
+const int prepinac2 = 11;
+unsigned long maxperioda;
+unsigned long minperioda;
 
 // PWM frekvence 50 Hz vyzaduje specifickou konfiguraci Timeru.
 
 void setup() {
-  pinMode(tlacitko, INPUT); // Nastavíme pin jako vstup
+  pinMode(tlacitko, INPUT); 
+  pinMode(prepinac1, INPUT_PULLUP);
+  pinMode(prepinac2, INPUT_PULLUP);
 
   // --- Konfigurace Timeru 1 pro 50 Hz PWM na Pinu 9 (Output Compare Pin A) ---
 
-  pinMode(9, OUTPUT); // Ujistime se, ze pin 9 je nastaven jako vystup
+  pinMode(PinPWM, OUTPUT); // Ujistime se, ze pin 9 je nastaven jako vystup
  
   // 1. Nastaveni rezimu: Fast PWM, TOP hodnota je ulozena v ICR1
   // WGM13:10 = 1110 (Fast PWM mode 14)
@@ -41,26 +50,37 @@ void setup() {
   // CS11 = 1 a CS10 = 1
   TCCR1B |= (1 << CS11) | (1 << CS10);
   
-   Serial.begin(9600); // Odkomentujte pro ladeni
+   Serial.begin(9600); 
    
 }
 
 void loop() {
   bool stavTlacitka=digitalRead(tlacitko);
   // Cteni hodnoty z potenciometru (rozsah 0 - 1023)
-  int senzorHodnotaPer = analogRead(potPinPer);
-  long Hodnota = map(senzorHodnotaPer, 0, 1023, 3000, 6000);
-
+  int senzorHodnotaPer = analogRead(pinPerioda);
+  maxperioda=6000;
+  minperioda=3000;
+  if (digitalRead(prepinac1)==LOW){
+    // 10x pomalejší
+    maxperioda=60000;
+    minperioda=30000;
+  }
+  if (digitalRead(prepinac2)==LOW){
+    // 10x rychlejší
+    maxperioda=600;
+    minperioda=300;
+  } 
+  
+  long periodaHodnota = map(senzorHodnotaPer, 0, 1023, minperioda, maxperioda);
+  ICR1 = periodaHodnota;
   // Cteni hodnoty z potenciometru (rozsah 0 - 1023)
-  int senzorHodnota = analogRead(potPin);
+  int senzorHodnota = analogRead(pinStrida);
   long pwmHodnota = map(senzorHodnota, 0, 1023, 0, ICR1);
   
   if (stavTlacitka==0){
-    ICR1 = Hodnota;
     OCR1A = pwmHodnota;
   } else {
-    ICR1 = Hodnota;
-    OCR1A = Hodnota-pwmHodnota;    
+    OCR1A = periodaHodnota-pwmHodnota;    
   }
   
  
@@ -70,6 +90,7 @@ void loop() {
    Serial.print(ICR1);
    Serial.print("  |  OCR1A (Duty Cycle Register): ");
    Serial.println(OCR1A);
+   delay(100);
   #endif
-   delay(50); // Kratka prodleva pro stabilizaci cteni
+   delay(10); // Kratka prodleva pro stabilizaci cteni
 }
